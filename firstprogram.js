4bc32d44b6d9c23947df73da3f4cf71715f0ab2e
Sriman2602 <77726570+Sriman2602@users.noreply.github.com>
2023-12-18 12:33:38 +0530

    Create firstprogram.js

---
+++ firstprogram.js
@@ -0,0 +1,11 @@
+const nemo = ['nemo'];
+
+function findnemo(array){
+  for(let i=0;i<nemo.length;i++){
+    if(array[i] == 'nemo'){
+      console.log("Found NEMO!");
+    }
+  }
+}
+
+findnemo(nemo);
