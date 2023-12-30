5fdc90334d4ab481c65eff9d6fa6857b4db72572
Sriman2602 <77726570+Sriman2602@users.noreply.github.com>
2023-12-30 13:50:46 +0530

    Create StringReverse.js

---
+++ 12 Algorithms: Recursion/03 Reverse String /StringReverse.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("yoyo master"));
+
+function reverseStringRecursive(str) {
+  if (str === "") {
+    return "";
+  }
+  return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+}
+
+console.log(reverseStringRecursive("yoyo master"));
